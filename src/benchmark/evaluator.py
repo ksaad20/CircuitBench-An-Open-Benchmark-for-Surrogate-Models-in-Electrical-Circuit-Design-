4df5d073711cfec7ff089856344b372a5aa67b0c
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 09:13:46 +0600

    Implement Evaluator class for model evaluation

--- src/benchmark/evaluator.py
+++ src/benchmark/evaluator.py
@@ -1,1 +1,37 @@
+"""
+Evaluator.
+"""
 
+from .metrics import *
+from .scorer import CompositeScore
+
+
+class Evaluator:
+
+    def __init__(self):
+
+        self.scorer=CompositeScore()
+
+    def evaluate(self,
+                 y_true,
+                 y_pred):
+
+        results={}
+
+        results["MAE"]=mae(y_true,y_pred)
+
+        results["MSE"]=mse(y_true,y_pred)
+
+        results["RMSE"]=rmse(y_true,y_pred)
+
+        results["R2"]=r2(y_true,y_pred)
+
+        results["MAPE"]=mape(y_true,y_pred)
+
+        results["Score"]=self.scorer.score(
+            results["RMSE"],
+            results["MAE"],
+            results["R2"]
+        )
+
+        return results

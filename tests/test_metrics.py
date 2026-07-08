74df7fcaf83f5a5e2b161c6dcd20b85cac3a7b72
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 08:14:44 +0600

    Add tests for mean squared error calculation

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,1 +1,26 @@
+"""
+Metric calculation tests.
+"""
 
+import numpy as np
+
+
+def mse(a, b):
+
+    return np.mean((a - b) ** 2)
+
+
+def test_zero_error():
+
+    x = np.array([1, 2, 3])
+
+    assert mse(x, x) == 0
+
+
+def test_positive_error():
+
+    x = np.array([1, 2])
+
+    y = np.array([3, 4])
+
+    assert mse(x, y) > 0

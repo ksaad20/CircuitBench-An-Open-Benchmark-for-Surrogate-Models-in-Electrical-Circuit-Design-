c7d53a7a960963aa3604034948eef0c19fb4db43
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 08:16:00 +0600

    Implement linear regression fit test
    Add a test for linear regression model fitting and prediction.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,1 +1,21 @@
+"""
+Machine-learning model tests.
+"""
 
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+
+def test_linear_regression_fit():
+
+    X = np.array([[1], [2], [3]])
+
+    y = np.array([2, 4, 6])
+
+    model = LinearRegression()
+
+    model.fit(X, y)
+
+    prediction = model.predict([[4]])
+
+    assert prediction[0] > 7

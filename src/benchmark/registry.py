1d3aba0208cf6e19f22e01504d7d8bdee8357cce
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 09:16:29 +0600

    Add Registry class for benchmark dataset management

--- src/benchmark/registry.py
+++ src/benchmark/registry.py
@@ -1,1 +1,21 @@
+"""
+Benchmark registry.
+"""
 
+class Registry:
+
+    def __init__(self):
+
+        self.datasets={}
+
+    def register(self,name,dataset):
+
+        self.datasets[name]=dataset
+
+    def list(self):
+
+        return list(self.datasets.keys())
+
+    def get(self,name):
+
+        return self.datasets[name]

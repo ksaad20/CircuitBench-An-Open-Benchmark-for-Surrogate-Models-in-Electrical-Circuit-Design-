d5f4a654d6ce62478ff204a5b6f95b56836e1d7d
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 09:13:03 +0600

    Implement RankingEngine class for ranking data

--- src/benchmark/ranking.py
+++ src/benchmark/ranking.py
@@ -1,1 +1,21 @@
+"""
+Ranking Engine.
+"""
 
+import pandas as pd
+
+
+class RankingEngine:
+
+    def rank(self,dataframe):
+
+        df=dataframe.copy()
+
+        df=df.sort_values(
+            by="Score",
+            ascending=False
+        )
+
+        df["Rank"]=range(1,len(df)+1)
+
+        return df

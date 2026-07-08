e73ee634f8810c9d8a0f0323d8514a5f6bd0b51e
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 10:48:25 +0600

    Implement memory benchmarking functions
    Added functions to start, stop, and retrieve memory usage statistics.

--- src/metrics/memory.py
+++ src/metrics/memory.py
@@ -1,1 +1,23 @@
+"""
+Memory benchmarking.
+"""
 
+import tracemalloc
+
+
+def start():
+    tracemalloc.start()
+
+
+def stop():
+    tracemalloc.stop()
+
+
+def current_memory():
+    current, peak = tracemalloc.get_traced_memory()
+    return current
+
+
+def peak_memory():
+    current, peak = tracemalloc.get_traced_memory()
+    return peak

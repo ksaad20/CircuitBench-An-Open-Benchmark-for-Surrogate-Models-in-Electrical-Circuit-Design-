eb60e1227dbcd615024ca0dd80fdd6da050d8d17
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-08 08:23:15 +0600

    Implement correlation and error heatmap functions
    Added functions to generate correlation and error heatmaps using matplotlib.

--- src/visualization/heatmaps.py
+++ src/visualization/heatmaps.py
@@ -1,1 +1,19 @@
+"""
+Generate correlation and error heatmaps.
+"""
 
+import matplotlib.pyplot as plt
+
+
+def correlation_heatmap(matrix):
+    plt.imshow(matrix)
+    plt.colorbar()
+    plt.title("Correlation Matrix")
+    plt.show()
+
+
+def error_heatmap(error_matrix):
+    plt.imshow(error_matrix)
+    plt.colorbar()
+    plt.title("Prediction Error")
+    plt.show()

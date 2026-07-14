63ef0aec555df823f995aa093edf2821e24ba2b9
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-14 12:18:07 +0600

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -17,12 +17,4 @@ def test_help_message():
     assert result.returncode == 0
 
 
-def test_cli_runs():
-
-    result = subprocess.run(
-        [sys.executable, "-m", "src.cli"],
-        capture_output=True,
-        text=True,
-    )
-
     assert result.returncode == 0
